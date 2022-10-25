56c8dc04fe13592a5c410ae96b0b385e0b99dfda
mutiso202 <62766629+mutiso202@users.noreply.github.com>
2022-10-25 17:42:22 +0300

    incident design commit

--- app/src/main/java/com/example/smartpatroladmin/LoginScreen.java
+++ app/src/main/java/com/example/smartpatroladmin/LoginScreen.java
@@ -2,9 +2,6 @@ package com.example.smartpatroladmin;
 
 import static com.example.smartpatroladmin.util.AppSystem.redirectActivity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
@@ -14,9 +11,8 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.smartpatroladmin.Helpers.GuardHelper;
-import com.example.smartpatroladmin.Interface.onResult;
-import com.example.smartpatroladmin.Models.Guard;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.smartpatroladmin.util.LoadingDialog;
 
 public class LoginScreen extends AppCompatActivity {
